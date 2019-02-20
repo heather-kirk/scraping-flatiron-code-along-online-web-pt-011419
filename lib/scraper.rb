f2b30ec8d72460bc3f9e7cfcfa8098219ab9f905
Heather Kirk <hskirk@gmail.com>
2019-02-20 02:58:38 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -16,10 +16,13 @@ class Scraper
  end 
  
  def make_courses
-   
+   self.get_courses.each do |post|
+    course = Course.new
+    course.title = post.css("h2").text
+    course.schedule = post.css(".date").text
+    course.description = post.css("p").text
+  end
  end 
- 
-  Scraper.new.get_page
   
     def print_courses
     self.make_courses
@@ -32,6 +35,7 @@ class Scraper
     end
   end
 
+Scraper.new.print_courses
 
 
 
